d7f21ae1d7d22437bf59351050cee4174322a0fc
Mayank <mbapna123@gmail.com>
2017-10-14 00:09:49 -0400

    fi

--- ServerSide.js
+++ ServerSide.js
@@ -25,13 +25,38 @@ app.use(cors());
 //     console.log("Database created");
 //   });
 // })
-
 var condatabase=mysql.createConnection({
   host: "us-cdbr-iron-east-05.cleardb.net",
   user: "b3f6181b95101f",
   password: "6079974d",
   database: "heroku_88c51e6c2ac50c3"
 })
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(condatabase); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
+
 //
 // condatabase.connect(function(err) {
 //   if (err) throw err;
